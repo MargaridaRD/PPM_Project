190000c41d67ef8e0de1d4890a6cfb15d1c709ef
rregada <ritaregada@gmail.com>
2021-04-09 15:35:56 +0100

    Verify pixels "composto"

--- src/projeto/Tree.scala
+++ src/projeto/Tree.scala
@@ -17,13 +17,18 @@ case class Tree(myField: QTree[Coords]) {
     lst map (x => x.toList)
   }
   //verifica se todos os pixeis são iguais para um quadrante
-  @tailrec
+
   def verify_pixels(lst:List[List[Int]], p:Int):Boolean={
-     lst match{
-     case Nil => true
-     case List(List(_))=> true
-     case y::ys  =>   println("y: " + y + "p: " + p )
-       if( y.takeWhile(x=> x==p).length == y.length) verify_pixels(ys,p) else false }}
+  @tailrec
+    def aux(l:List[Int]):Boolean = {
+      l match {
+        case Nil => true
+        case y :: ys :: yss => if (y == ys) aux(ys::yss) else false
+      }
+
+    }
+    aux(lst.flatten)
+  }
 
   // length -1 nao é preciso porque é do quadrante a seguir
   def makeTree(lst: List[List[Int]], p:Point): QTree[Coords] = {
@@ -32,14 +37,14 @@ case class Tree(myField: QTree[Coords]) {
       case _ =>
         if( verify_pixels(lst, lst.head.head))  {
           // QLeaf[Coords, Section] = QLeaf((((0,0):Point,(0,0):Point):Coords, Color.red):Section)
-          QLeaf( (p,(p._1 + lst(1).length,p._2 + lst.length)), ImageUtil.decodeRgb(lst.head.head))
+          QLeaf( ((p,(p._1 + lst.head.length - 1, p._2 + lst.length - 1)), ImageUtil.decodeRgb(lst.head.head)))
         }
         else {
-          QNode((p, (p._1 + lst(1).length - 1, p._2 + lst.length - 1)),
+          QNode((p, (p._1 + lst.head.length - 1, p._2 + lst.length - 1)),
             makeTree(verticalSlice(horizontalSlice(lst)._1)._1, p),
-            makeTree(verticalSlice(horizontalSlice(lst)._1)._2, (p._1 + lst(1).length / 2, p._2)),
+            makeTree(verticalSlice(horizontalSlice(lst)._1)._2, (p._1 + lst.head.length / 2, p._2)),
             makeTree(verticalSlice(horizontalSlice(lst)._2)._1, (p._1, p._2 + lst.length / 2)),
-            makeTree(verticalSlice(horizontalSlice(lst)._2)._2, (p._1 + lst(1).length / 2, p._2 + lst.length / 2)))
+            makeTree(verticalSlice(horizontalSlice(lst)._2)._2, (p._1 + lst.head.length / 2, p._2 + lst.length / 2)))
         }
     }
   }
@@ -47,17 +52,15 @@ case class Tree(myField: QTree[Coords]) {
 
   def horizontalSlice(lst: List[List[Int]]): (List[List[Int]], List[List[Int]]) = {
     lst match {
-      case List(List()) => (Nil, Nil)
+      case List() => (Nil, Nil)
       case xss => xss.splitAt(xss.length/2)
     }
   }
   def verticalSlice(lst: List[List[Int]]): (List[List[Int]], List[List[Int]]) = {
 
     lst match {
-      case List(List()) => (Nil, Nil)
-      case xss => xss.splitAt(xss.length/2)
+      case List() => (Nil, Nil)
       case xs::xss => (xs.splitAt(xs.length/2)._1 :: verticalSlice(xss)._1, xs.splitAt(xs.length/2)._2 :: verticalSlice(xss)._2)
-
     }
   }
  /* def verticalSlice(lst: List[List[Int]]): (List[List[Int]], List[List[Int]]) = {
@@ -73,9 +76,8 @@ case class Tree(myField: QTree[Coords]) {
 
   }*/
  def main(args: Array[String]): Unit = {
-   val array = ImageUtil.readColorImage("src/projeto/objc2_2.png")
-
-   val teste= makeTree(makeList(array),(0,0))
+   val array = makeList( ImageUtil.readColorImage("src/projeto/batata.png"))
+   val teste= makeTree(array,(0,0))
    println("teste: " + teste )
  }
 
