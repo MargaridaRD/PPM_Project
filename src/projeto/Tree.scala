56fb0ad9c0e78def487cb704f6239acbc4346018
MargaridaRD <72627134+MargaridaRD@users.noreply.github.com>
2021-04-09 15:40:56 +0100

    pixel

--- src/projeto/Tree.scala
+++ src/projeto/Tree.scala
@@ -10,7 +10,7 @@ case class Tree(myField: QTree[Coords]) {
 
 }
 
-  object Tree{
+object Tree{
 
   def makeList(ar: Array[Array[Int]]): List[List[Int]] = {
     val lst = ar.toList
@@ -30,16 +30,16 @@ case class Tree(myField: QTree[Coords]) {
     lst match {
       case Nil => QEmpty
       case _ =>
-        if( verify_pixels(lst, lst.head.head))  {
+        if( verify_pixels(lst))  {
           // QLeaf[Coords, Section] = QLeaf((((0,0):Point,(0,0):Point):Coords, Color.red):Section)
-          QLeaf( (p,(p._1 + lst(1).length,p._2 + lst.length)), ImageUtil.decodeRgb(lst.head.head))
+          QLeaf( ((p,(p._1 + lst.head.length - 1, p._2 + lst.length - 1)), ImageUtil.decodeRgb(lst.head.head)))
         }
         else {
-          QNode((p, (p._1 + lst(1).length - 1, p._2 + lst.length - 1)),
+          QNode((p, (p._1 + lst.head.length - 1, p._2 + lst.length - 1)),
             makeTree(verticalSlice(horizontalSlice(lst)._1)._1, p),
-            makeTree(verticalSlice(horizontalSlice(lst)._1)._2, (p._1 + lst(1).length / 2, p._2)),
+            makeTree(verticalSlice(horizontalSlice(lst)._1)._2, (p._1 + lst.head.length / 2, p._2)),
             makeTree(verticalSlice(horizontalSlice(lst)._2)._1, (p._1, p._2 + lst.length / 2)),
-            makeTree(verticalSlice(horizontalSlice(lst)._2)._2, (p._1 + lst(1).length / 2, p._2 + lst.length / 2)))
+            makeTree(verticalSlice(horizontalSlice(lst)._2)._2, (p._1 + lst.head.length / 2, p._2 + lst.length / 2)))
         }
     }
   }
@@ -47,37 +47,23 @@ case class Tree(myField: QTree[Coords]) {
 
   def horizontalSlice(lst: List[List[Int]]): (List[List[Int]], List[List[Int]]) = {
     lst match {
-      case List(List()) => (Nil, Nil)
+      case List() => (Nil, Nil)
       case xss => xss.splitAt(xss.length/2)
     }
   }
   def verticalSlice(lst: List[List[Int]]): (List[List[Int]], List[List[Int]]) = {
 
     lst match {
-      case List(List()) => (Nil, Nil)
-      case xss => xss.splitAt(xss.length/2)
+      case List() => (Nil, Nil)
       case xs::xss => (xs.splitAt(xs.length/2)._1 :: verticalSlice(xss)._1, xs.splitAt(xs.length/2)._2 :: verticalSlice(xss)._2)
-
     }
   }
- /* def verticalSlice(lst: List[List[Int]]): (List[List[Int]], List[List[Int]]) = {
-    def aux (l: List[List[Int]], f: (List[List[Int]], List[List[Int]])): (List[List[Int]], List[List[Int]]) = {
-      l match {
-        case List(List()) => f
-        case xss => xss.splitAt(xss.length / 2)
-        case xs :: xss => aux(xss, (((xs.splitAt(xs.length / 2))._1 :: f._1), ((xs.splitAt(xs.length / 2))._2 :: f._2)))
 
-      }
-    }
-    aux(lst, (Nil,Nil))
-
-  }*/
- def main(args: Array[String]): Unit = {
-   val array = ImageUtil.readColorImage("src/projeto/objc2_2.png")
-
-   val teste= makeTree(makeList(array),(0,0))
-   println("teste: " + teste )
- }
+  def main(args: Array[String]): Unit = {
+    val array = makeList( ImageUtil.readColorImage("src/projeto/objc2_2.png"))
+    val teste= makeTree(array,(0,0))
+    println("teste: " + teste )
+  }
 
 
 }
