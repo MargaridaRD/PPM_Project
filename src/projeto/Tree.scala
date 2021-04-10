088be12addc1607cfa22aad4e96a34a00e1333a9
MargaridaRD <72627134+MargaridaRD@users.noreply.github.com>
2021-04-10 19:51:39 +0100

    mirrorV & finito BitMap

--- src/projeto/Tree.scala
+++ src/projeto/Tree.scala
@@ -6,16 +6,16 @@ import projeto.Section.Section
 import scala.annotation.tailrec
 
 
- case class Tree(myField: QTree[Coords]) {
+case class Tree(myField: QTree[Coords]) {
 
 }
 
-  object Tree{
+object Tree{
 
-//MAKE TREE
+  //MAKE TREE
   //verifica se todos os pixeis são iguais para um quadrante
   def verify_pixels(lst:List[List[Int]]):Boolean={
-  @tailrec
+    @tailrec
     def aux(l:List[Int]):Boolean = {
       l match {
         case Nil => true
@@ -26,25 +26,26 @@ import scala.annotation.tailrec
 
     aux(lst.flatten)
   }
-    def horizontalSlice(lst: List[List[Int]]): (List[List[Int]], List[List[Int]]) = {
-      lst match {
-        case List() => (Nil, Nil)
-        case xss => xss.splitAt(xss.length/2)
-      }
+  def horizontalSlice(lst: List[List[Int]]): (List[List[Int]], List[List[Int]]) = {
+    lst match {
+      case List() => (Nil, Nil)
+      case xss => xss.splitAt(xss.length/2)
     }
-    def verticalSlice(lst: List[List[Int]]): (List[List[Int]], List[List[Int]]) = {
+  }
+  def verticalSlice(lst: List[List[Int]]): (List[List[Int]], List[List[Int]]) = {
 
-      lst match {
-        case List() => (Nil, Nil)
-        case xs::xss => (xs.splitAt(xs.length/2)._1 :: verticalSlice(xss)._1, xs.splitAt(xs.length/2)._2 :: verticalSlice(xss)._2)
-      }
+    lst match {
+      case List() => (Nil, Nil)
+      case xs::xss => (xs.splitAt(xs.length/2)._1 :: verticalSlice(xss)._1, xs.splitAt(xs.length/2)._2 :: verticalSlice(xss)._2)
     }
+  }
 
   // length -1 nao é preciso em todos porque é do quadrante a seguir
   def makeTree(ar: Array[Array[Int]]): QTree[Coords] = {
     def aux(lst: List[List[Int]], p: Point): QTree[Coords] = {
       lst match {
         case Nil => QEmpty
+        case List(List())=> QEmpty
         case _ =>
           if (verify_pixels(lst)) {
             QLeaf(((p, (p._1 + lst.head.length - 1, p._2 + lst.length - 1)), ImageUtil.decodeRgb(lst.head.head).toList))
@@ -58,51 +59,62 @@ import scala.annotation.tailrec
           }
       }
     }
-  aux(ar.toList map (x => x.toList),(0,0))
+    aux(ar.toList map (x => x.toList),(0,0))
   }
 
- //MAKE BITMAP
+  //MAKE BITMAP
 
-    // glue junta verticalmente l1+l2 e l3+l4 e junta horizontalmente o resultado das duas
-    def glue(l1: List[List[Int]], l2: List[List[Int]],l3: List[List[Int]], l4: List[List[Int]]): List[List[Int]] = {
+  // glue junta verticalmente l1+l2 e l3+l4 e junta horizontalmente o resultado das duas
+  def glue(l1: List[List[Int]], l2: List[List[Int]],l3: List[List[Int]], l4: List[List[Int]]): List[List[Int]] = {
 
-      def glue_vertical(l1: List[List[Int]], l2: List[List[Int]]): List[List[Int]] = {
-        (l1, l2) match {
-          case (Nil, Nil) => Nil
-          case (_, Nil) => l1
-          case (Nil, _) => l2
-          case (x :: xs, y :: ys) => (x foldRight y) (_:: _) :: glue_vertical(xs, ys)
-        }
+    def glue_vertical(l1: List[List[Int]], l2: List[List[Int]]): List[List[Int]] = {
+      (l1, l2) match {
+        case (Nil, Nil) => Nil
+        case (_, Nil) => l1
+        case (Nil, _) => l2
+        case (x :: xs, y :: ys) => (x foldRight y) (_:: _) :: glue_vertical(xs, ys)
       }
-        glue_vertical(l1, l2) ::: glue_vertical(l3, l4)
     }
+    glue_vertical(l1, l2) ::: glue_vertical(l3, l4)
+  }
   def leafToList(section: Section): List[List[Int]] = {
-    val numPix: Int = section._1._2._1 - section._1._1._1 +1
+    val x: Int = section._1._2._1 - section._1._1._1 + 1
+    val y: Int = section._1._2._2 - section._1._1._2 + 1
     val cor = ImageUtil.encodeRgb(section._2.head,section._2(1),section._2(2))
-    List.fill(numPix*numPix)(cor).grouped(numPix).toList
+    List.fill(x*y)(cor).grouped(x).toList
   }
 
-    def makeBitMap(qTree:QTree[Coords]):Array[Array[Int]]= {
-      def aux(qTree:QTree[Coords]):List[List[Int]]= {
-        qTree match {
-          case QEmpty => Nil
-          case QLeaf(s: Section) => leafToList(s)
-          case QNode(_, l1, l2, l3, l4) => glue(aux(l1), aux(l2), aux(l3), aux(l4))
+  def makeBitMap(qTree:QTree[Coords]):Array[Array[Int]]= {
+    def aux(qTree:QTree[Coords]):List[List[Int]]= {
+      qTree match {
+        case QEmpty => Nil
+        case QLeaf(s: Section) => leafToList(s)
+        case QNode(_, l1, l2, l3, l4) => glue(aux(l1), aux(l2), aux(l3), aux(l4))
 
-        }
       }
-      aux(qTree).toArray map (x => x.toArray)
-
     }
+    aux(qTree).toArray map (x => x.toArray)
+
+  }
+  /*def mirrorV (qt:QTree[Coords]):QTree[Coords]={
+        qt match {
+          case QEmpty => qt
+          case QNode(a, l1, l2, l3, l4) => {
+
+             mirrorV(QNode(a, l2, l1, l4, l3))
+           }
+        }
+  }
+*/
 
 
 
+  def main(args: Array[String]): Unit = {
+    val teste = makeTree( ImageUtil.readColorImage("src/projeto/img/objc2_2.png"))
 
- def main(args: Array[String]): Unit = {
-   val teste = makeTree( ImageUtil.readColorImage("src/projeto/img/retangulo.png"))
-   val teste2= ImageUtil.writeImage(makeBitMap(teste), "src/projeto/img/teste3.png", "png")
-   println("teste: " + teste )
- }
+   // val mirror_V = mirror(teste)
+    val teste2= ImageUtil.writeImage(makeBitMap(teste), "src/projeto/img/teste_mirrorV.png", "png")
+  }
 
 
 }
