b800a2e538029cce83d75dd786139b6ccad32f6b
MargaridaRD <72627134+MargaridaRD@users.noreply.github.com>
2021-04-12 19:27:43 +0100

    asdf

--- src/projeto/Main.scala
+++ src/projeto/Main.scala
@@ -2,32 +2,32 @@ package projeto
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val tree:Tree = Tree(ImageUtil.readColorImage("src/projeto/img/rita.jpeg"))
+    val tree:Tree = Tree(ImageUtil.readColorImage("src/projeto/img/169x300.png"))
     val teste_makeTree = tree.makeTree()
     val teste_makeBitMap = tree.makeBitMap(teste_makeTree)
     ImageUtil.writeImage(teste_makeBitMap, "src/projeto/img/teste_makeBitMap.png", "png")
 
     //////Efeitos////////////
-    val effects:Effects = Effects(teste_makeTree)
+    //val effects:Effects = Effects(teste_makeTree)
     //mirrorV
-    ImageUtil.writeImage(tree.makeBitMap(effects.mirrorV()), "src/projeto/img/mirrorV.png", "png")
+    //ImageUtil.writeImage(tree.makeBitMap(effects.mirrorV()), "src/projeto/img/mirrorV.png", "png")
 
     //mirrorH
-    ImageUtil.writeImage(tree.makeBitMap(effects.mirrorH()), "src/projeto/img/mirrorH.png", "png")
+    //ImageUtil.writeImage(tree.makeBitMap(effects.mirrorH()), "src/projeto/img/mirrorH.png", "png")
     //rotateR
-    ImageUtil.writeImage(tree.makeBitMap(effects.rotateR()), "src/projeto/img/rotateR.png", "png")
+    //ImageUtil.writeImage(tree.makeBitMap(effects.rotateR()), "src/projeto/img/rotateR.png", "png")
 
     //rotateL
-    ImageUtil.writeImage(tree.makeBitMap(effects.rotateL()), "src/projeto/img/rotateL.png", "png")
+    //ImageUtil.writeImage(tree.makeBitMap(effects.rotateL()), "src/projeto/img/rotateL.png", "png")
 
     //mapColorEffect
 
     //sepia
-    ImageUtil.writeImage(tree.makeBitMap(effects.mapColorEffect(effects.sepia)), "src/projeto/img/sepia.png", "png")
+   // ImageUtil.writeImage(tree.makeBitMap(effects.mapColorEffect(effects.sepia)), "src/projeto/img/sepia.png", "png")
     //noise
-    ImageUtil.writeImage(tree.makeBitMap(effects.mapColorEffect(effects.noise)), "src/projeto/img/noise.png", "png")
+   // ImageUtil.writeImage(tree.makeBitMap(effects.mapColorEffect(effects.noise)), "src/projeto/img/noise.png", "png")
     //contrast
-    ImageUtil.writeImage(tree.makeBitMap(effects.mapColorEffect(effects.contrast)), "src/projeto/img/contrast.png", "png")
+   // ImageUtil.writeImage(tree.makeBitMap(effects.mapColorEffect(effects.contrast)), "src/projeto/img/contrast.png", "png")
 
   }
 
