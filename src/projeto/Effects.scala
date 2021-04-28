b506980d044a8181ea7e2b7150967ba63bf868ff
MargaridaRD <72627134+MargaridaRD@users.noreply.github.com>
2021-04-28 18:14:23 +0100

    noise_puro

--- src/projeto/Effects.scala
+++ src/projeto/Effects.scala
@@ -19,6 +19,8 @@ case class Effects(myField:QTree[Coords]){
   def contrast (c:Color): Color=Effects.contrast(c)
   def sepia (c:Color): Color=Effects.sepia(c)
 
+  def mapColorEffect_1(f:(Color, RandomWithState) => (Color,RandomWithState), r:RandomWithState):QTree[Coords]= Effects.mapColorEffect_1(f,this.myField,r)
+  def noise_1 (c:Color, r:RandomWithState): (Color,RandomWithState)= Effects.noise_1(c,r)
 }
 
 object Effects {
@@ -161,6 +163,25 @@ object Effects {
       case QEmpty=>QEmpty
       case QLeaf(s:Section) =>QLeaf((s._1,f(s._2)))
       case  QNode(a, l1, l2, l3, l4)=>QNode(a, mapColorEffect(f,l1), mapColorEffect(f,l2), mapColorEffect(f,l3), mapColorEffect(f,l4))
+    }
+  }
+
+  //Noise puro
+  def noise_1 (c:Color, r:RandomWithState): (Color,RandomWithState)={
+    val random_noise= r.nextInt(122)
+
+    (List (validatecomponent(random_noise._1+c.head),
+      validatecomponent(random_noise._1+c(1)),
+      validatecomponent(random_noise._1+c(2))),random_noise._2)
+
+
+  }
+  def mapColorEffect_1(f:(Color, RandomWithState) => (Color,RandomWithState), qt:QTree[Coords], r:RandomWithState):QTree[Coords]={
+    qt match {
+      case QEmpty=>QEmpty
+      case QLeaf(s:Section) =>QLeaf((s._1,f(s._2,r)._1))
+      case  QNode(a, l1, l2, l3, l4)=>QNode(a, mapColorEffect_1(f,l1,r), mapColorEffect_1(f,l2,r),
+        mapColorEffect_1(f,l3,r), mapColorEffect_1(f,l4,r))
     }
   }
 
--- src/projeto/Main.scala
+++ src/projeto/Main.scala
@@ -9,21 +9,25 @@ object Main {
     //////Efeitos////////////
     val effects:Effects = Effects(teste_makeTree)
     //mirrorV
-    tree.treeToImage( "src/projeto/img/mirrorV.png", "png",effects.mirrorV())
+    //tree.treeToImage( "src/projeto/img/mirrorV.png", "png",effects.mirrorV())
     //mirrorH
-    tree.treeToImage( "src/projeto/img/mirrorH.png", "png",effects.mirrorH())
+    //tree.treeToImage( "src/projeto/img/mirrorH.png", "png",effects.mirrorH())
     //rotateR
-    tree.treeToImage( "src/projeto/img/rotateR.png", "png",effects.rotateR())
+   // tree.treeToImage( "src/projeto/img/rotateR.png", "png",effects.rotateR())
     //rotateL
-    tree.treeToImage( "src/projeto/img/rotateR.png", "png",effects.rotateL())
+    //tree.treeToImage( "src/projeto/img/rotateR.png", "png",effects.rotateL())
     //scale
-    tree.treeToImage( "src/projeto/img/scale.png", "png",effects.scale(0.5))
+    //tree.treeToImage( "src/projeto/img/scale.png", "png",effects.scale(0.5))
     //sepia
-    tree.treeToImage( "src/projeto/img/sepia.png", "png",effects.mapColorEffect(effects.sepia))
+    //tree.treeToImage( "src/projeto/img/sepia.png", "png",effects.mapColorEffect(effects.sepia))
     //noise
-    tree.treeToImage( "src/projeto/img/noise.png", "png",effects.mapColorEffect(effects.noise))
+    //tree.treeToImage( "src/projeto/img/noise.png", "png",effects.mapColorEffect(effects.noise))
     //contrast
-    tree.treeToImage( "src/projeto/img/contrast.png", "png",effects.mapColorEffect(effects.contrast))
+   // tree.treeToImage( "src/projeto/img/contrast.png", "png",effects.mapColorEffect(effects.contrast))
+
+    //noise_puro
+    val r = MyRandom(11)
+    tree.treeToImage( "src/projeto/img/noise.png", "png",effects.mapColorEffect_1(effects.noise_1,r ))
 
   }
 
